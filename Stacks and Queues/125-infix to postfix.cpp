f99b43339c926ec750ce98973e9473ab7e82dfaa
msabhinavchandra <116635275+msabhinavchandra@users.noreply.github.com>
2024-01-14 16:55:43 +0530

    q

--- Stacks and Queues/125-infix to postfix.cpp
+++ Stacks and Queues/125-infix to postfix.cpp
@@ -193,3 +193,109 @@ int main()
 }
 //output:
 // abc/+kj/w-*
+
+// in code-studio problem there is not even a word which was raised regarding the associativity but it is good to have an idea 
+// regarding the associativity in conversion of infix to postfix, for which you can refer kpvr's code which always
+// good and well-written
+
+// in the actual problem out thereas there is no question regarding the associativity, the associativity is only checked once
+// the precedence of both the operators are same and equal, but in this case, the associativity is not at all checked
+
+// everything in the problem went according to the assumption that right to left associativity
+// all the operators are popped out from the stack even when the precedence of the curr is lesser than equal to the top of the 
+// stack
+
+//means i was actually wrong in the above, for all the operators , the following code will follow the same precedence, that is
+//left to write associativity , even for the exponent(but in real,the exponent will have the associativity from right to left,
+// you keep on adding elements into the stack, even when the precedence id equal,but in the case of associativity of left to 
+// right you will pop the elements if you find the curr is lesser than or equal to the top of the stack)
+// so that this left to right precedence is retained.
+
+// that alternative code i was talking about.
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include <stack>
+
+
+
+
+int Precedence(char c)
+{
+
+    if (c == '^')
+    {
+        return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+char Associativity(char c){
+
+    if(c=='^'){
+        return 'R';
+    }
+    return 'L';
+}
+
+
+
+string infixToPostfix(string s){
+
+    
+    string result="";
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+
+        if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')||(s[i]>='0' && s[i]<='9'))
+        {
+            result += (s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                result += st.top();
+                st.pop();
+            }
+            if (!st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && Precedence(st.top()) >= Precedence(s[i]))//so that the left to right associativity is retained.
+            {                                                              //in the case of R ass..ty you will push it into the stack, in that
+                                                                            // case so  that R ass..ty is retained.
+                result += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()){
+        result+=st.top();
+        st.pop();
+    }
+    return result;
+	
+}
