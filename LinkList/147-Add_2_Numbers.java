2d51d489fed21284ad891b0ea4484b4488cf0dac
msabhinavchandra <116635275+msabhinavchandra@users.noreply.github.com>
2024-04-25 21:09:03 +0530

    donefortoday

--- LinkList/147-Add_2_Numbers.java
+++ LinkList/147-Add_2_Numbers.java
@@ -1,9 +1,17 @@
-// there is no brute force
-//there is only optimal for this problem
-//so ask interviewer more questions to pass more time.
+//there is no brute 
+//the way, the reason this works the way it does because 
+//when we are normally adding any 2 numbers
+// we will start by adding the least significant digits and then move to the 
+//msb's when then we go on taking the carry on carry and by carry
+//so when we are addign these 2 linkedlists we are getting started by adding from the lsb's 
+//and then go on moving on till the msb's and then carry
+//so that is the reason for this question the way,this way of approach works for this question
+//that is normally adding from the lsb's to the msb's and then carry.
 
-//i found 1 brute , where i am getting stack overflow this is the case with recursion as reversing the 
-//LinkedLists.
+// Time Complexity: O(max(m,n)). Assume that m and n represent the length of l1 and l2 respectively,
+//  the algorithm above iterates at most max(m,n) times.
+
+// Space Complexity: O(max(m,n)). The length of the new list is at most max(m,n)+1.
 
 import java.util.* ;
 import java.io.*; 
@@ -25,32 +33,32 @@ import java.io.*;
 
 public class Solution {
 
-    static LinkedListNode reverseLinkedList(LinkedListNode head){
-        if(head==null || head.next==null)
-        {
-            return head;
-        }
-        LinkedListNode newHead=reverseLinkedList(head.next);
-        LinkedListNode front=head.next;
-        front.next=head;
-        head.next=null;
-        return newHead;
-    }
+    // static LinkedListNode reverseLinkedList(LinkedListNode head){
+    //     if(head==null || head.next==null)
+    //     {
+    //         return head;
+    //     }
+    //     LinkedListNode newHead=reverseLinkedList(head.next);
+    //     LinkedListNode front=head.next;
+    //     front.next=head;
+    //     head.next=null;
+    //     return newHead;
+    // }
 
     static LinkedListNode Add2LinkedLists(LinkedListNode thead1,LinkedListNode thead2)
     {
         LinkedListNode temp1=thead1;
         LinkedListNode temp2=thead2;
 
-        LinkedListNode newHead1=reverseLinkedList(temp1);
-        LinkedListNode newHead2=reverseLinkedList(temp2);
+        LinkedListNode newHead1=temp1;
+        LinkedListNode newHead2=temp2;
 
         LinkedListNode dummy1=new LinkedListNode(-1);
         LinkedListNode traverseHead=null;
         int sum=0;
         int carry=0;
 
-        while(newHead1!=null || newHead2!=null)
+        while(newHead1!=null && newHead2!=null)
         {
             sum=newHead1.data+newHead2.data+carry;
             int newNodeData=sum%10;
@@ -111,6 +119,11 @@ public class Solution {
 
         }
 
+        if(carry==1){
+            LinkedListNode newNode=new LinkedListNode(carry);
+            traverseHead.next=newNode;
+        }
+
         return dummy1.next;
 
     }
@@ -119,9 +132,7 @@ public class Solution {
         LinkedListNode t1=head1;
         LinkedListNode t2=head2;
         LinkedListNode temp=Add2LinkedLists(t1, t2);
-        LinkedListNode result=reverseLinkedList(temp);
-        return result;
-
-
+        // LinkedListNode result=reverseLinkedList(temp);
+        return temp;
     }
 }
