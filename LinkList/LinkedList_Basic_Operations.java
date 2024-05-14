f3daff0fb01a16fd8ec54ee637a8142c94257f68
msabhinavchandra <116635275+msabhinavchandra@users.noreply.github.com>
2024-05-14 08:01:02 +0530

    s

---
+++ LinkList/LinkedList_Basic_Operations.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node() {
+        data = 0;
+        next = null;
+    }
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+
+    Node(int x, Node next) {
+        data = x;
+        this.next = next;
+    }
+}
+
+public class LinkedListOperations {
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static Node constructLL(List<Integer> arr) {
+        int n = arr.size();
+        Node head = new Node(arr.get(0));
+        Node temp = head;
+
+        for (int i = 1; i < n; i++) {
+            Node curNode = new Node(arr.get(i));
+            temp.next = curNode;
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+    public static Node addElementHead(Node head, int val) {
+        Node n = new Node(val);
+        n.next = head;
+        head = n;
+        return head;
+    }
+
+    public static Node addElementTail(Node head, int val) {
+        Node n = new Node(val);
+        
+        if (head == null) {
+            head = n;
+            return head;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = n;
+        return head;
+    }
+
+    public static Node deleteElement(Node head, int val) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head.data == val) {
+            Node toBeDeleted = head;
+            head = head.next;
+            toBeDeleted = null;
+            return head;
+        }
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data != val) {
+            temp = temp.next;
+        }
+
+        if (temp.next != null) {
+            Node toBeDeleted = temp.next;
+            temp.next = temp.next.next;
+            toBeDeleted = null;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(2, 5, 8, 7);
+        Node head = constructLL(arr);
+        display(head);
+
+        head = addElementTail(head, 1);
+        head = addElementTail(head, 2);
+        head = addElementTail(head, 3);
+        head = addElementTail(head, 4);
+        head = addElementTail(head, 5);
+        display(head);
+
+        head = deleteElement(head, 5);
+        head = addElementHead(head, 0);
+        display(head);
+    }
+}
---
+++ Stacks and Queues/Stack_Operations.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        // Create a stack of integers
+        Stack<Integer> stack = new Stack<>();
+
+        // Pushing elements onto the stack
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        // Popping an element from the stack
+        int topElement = stack.pop();
+        System.out.println("Popped element: " + topElement);
+
+        // Peeking at the top element
+        topElement = stack.peek();
+        System.out.println("Top element: " + topElement);
+
+        // Checking if the stack is empty
+        boolean isEmpty = stack.isEmpty();
+        System.out.println("Is stack empty? " + isEmpty);
+
+        // Finding the position of an element
+        int position = stack.search(20);
+        if (position != -1) {
+            System.out.println("Element found at position: " + position);
+        } else {
+            System.out.println("Element not found in the stack");
+        }
+
+        // Iterating over the stack
+        System.out.println("Stack elements:");
+        for (Integer element : stack) {
+            System.out.println(element);
+        }
+    }
+}
