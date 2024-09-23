1c4aed753762f0619c8409fe7649e779786859fe
M.S.Abhinav Chandra <116635275+msabhinavchandra@users.noreply.github.com>
2024-09-24 01:08:59 +0530

    Yes!!

--- Recursion_&_BackTracking/193-Prime_StroboGrammatic_Number_smallestandLargest.java
+++ Recursion_&_BackTracking/193-Prime_StroboGrammatic_Number_smallestandLargest.java
@@ -54,3 +54,64 @@ Sample Output-3:
 1001 9966
 
 NOTE: Print the results in sorted order to pass the testcases */
+
+//Code to be tested
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        int noOfDigits=sc.nextInt();
+        Queue<Integer> halfAns=new LinkedList<>();
+       
+        List<String> Reqresult=stroboGrammatic(noOfDigits,noOfDigits);
+        Collections.sort(Reqresult);
+        for(String s:Reqresult){
+            int typeConversion=Integer.parseInt(s);
+            if(isPrime(typeConversion)) halfAns.add(typeConversion);
+        }
+
+        System.out.println(halfAns);
+       
+        System.out.print(Reqresult.get(0)+" "+Reqresult.get(Reqresult.size()-1));
+        
+    }
+    
+    public static List<String> stroboGrammatic(int n,int m){
+        char[][] pairs={{'1','1'},{'6','9'},{'9','6'},{'8','8'}};
+        //Base conditions
+        if(n==0) return new ArrayList<>(Arrays.asList(""));
+        if(n==1) return new ArrayList<>(Arrays.asList("1","8","0"));
+        
+        List<String> reference=stroboGrammatic(n-2,m);
+        List<String> result=new ArrayList<>();
+        
+        for(int i=0;i<reference.size();i++){
+            if(n!=m){
+                result.add("0"+reference.get(i)+"0");
+            }
+            for(char[] pair:pairs){
+                result.add(pair[0]+reference.get(i)+pair[1]);
+            }
+        }
+        return result;
+    }
+    
+    public static boolean isPrime(int n){
+        int cnt=0;
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                cnt+=1;
+                if(n/i!=i) cnt+=1;
+                
+            }
+            if(cnt>2) break;
+        }
+        if(cnt==2) return true;
+        return false;
+    }
+    
+}
