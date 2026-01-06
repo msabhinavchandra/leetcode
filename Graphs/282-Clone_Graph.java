266dde8266f58c9f0b07259d909f1892f831448b
msabhinavchandra <msabhinavchandra@gmail.com>
2026-01-06 19:30:54 +0530

    Refactor cloneGraph implementation to improve node cloning logic and handle null cases

--- Graphs/282-Clone_Graph.java
+++ Graphs/282-Clone_Graph.java
@@ -1,4 +1,76 @@
+// Abhinav and Neetcode
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
 import java.util.*;
+
+class Solution2 {
+    HashMap<Node, Node> map;
+
+    public Node cloneGraph(Node node) {
+        map = new HashMap<>();
+        Node newnode = func(node);
+        return newnode;
+
+    }
+
+    public Node func(Node node) {
+        // if copy exists
+        if (node == null)
+            return null;
+        int n = node.val;
+        Node newnode = new Node(n);//calling only for 
+        map.put(node, newnode);
+
+        for (Node ngbr : node.neighbors) {
+            if (map.containsKey(ngbr)) {// if alr created
+                newnode.neighbors.add(map.get(ngbr));
+            }
+            // if uncreated
+            else {
+                // newnode.neighbors.add(map.get(ngbr));
+                func(ngbr);
+                newnode.neighbors.add(map.get(ngbr));
+
+            }
+
+        }
+        return newnode;
+    }
+    // return newnode;
+
+}
+// time complexity O(N+E) each node and edge is visited once
+// space complexity O(N) hashmap and recursion stack
+
+//Reason: Each node is visited once and each edge is traversed once while 
+// iterating through neighbors.
+/*  Because we use a HashMap to avoid revisiting nodes, each node is cloned only 
+once.
+And when cloning a node, we iterate through all its neighbors, so across the graph
+every edge is processed once. */
+
+
+
+// import java.util.*;
 
 // Definition for a Node.
 class Node {
